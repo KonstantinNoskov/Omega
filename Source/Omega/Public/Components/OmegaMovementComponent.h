06e2debc593fafb540d192007450712cd165f62c
KNoskov <kosinosweb@gmail.com>
2025-02-04 14:30:36 +0500

    init commit

---
+++ Source/Omega/Public/Components/OmegaMovementComponent.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "OmegaMovementComponent.generated.h"
+
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class OMEGA_API UOmegaMovementComponent : public UCharacterMovementComponent
+{
+	GENERATED_BODY()
+
+public:
+
+	UOmegaMovementComponent();
+
+protected:
+
+	virtual void BeginPlay() override;
+
+public:
+
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+};
