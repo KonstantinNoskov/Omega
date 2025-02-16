d6216a17b0495bec6fe1f0f948cc00733072a048
KNoskov <kosinosweb@gmail.com>
2025-02-16 15:05:49 +0500

    Init commit

---
+++ Source/Omega/Public/AbilitySystem/Abilities/OmegaGameplayAbility.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/GameplayAbility.h"
+#include "OmegaGameplayAbility.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class OMEGA_API UOmegaGameplayAbility : public UGameplayAbility
+{
+	GENERATED_BODY()
+};
