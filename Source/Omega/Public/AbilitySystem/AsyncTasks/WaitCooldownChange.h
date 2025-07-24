b09134972efce1ed7b2e49f01bcbb7c596258d39
Konstantin Noskov <noskovjr@yandex.ru>
2025-07-24 23:22:50 +0500

    WaitForCooldown async task

---
+++ Source/Omega/Public/AbilitySystem/AsyncTasks/WaitCooldownChange.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayEffect.h"
+#include "GameplayTagContainer.h"
+#include "Kismet/BlueprintAsyncActionBase.h"
+#include "WaitCooldownChange.generated.h"
+
+class UAbilitySystemComponent;
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FCooldownChangedSignature, float, TimeRemaining);
+
+UCLASS()
+class OMEGA_API UWaitCooldownChange : public UBlueprintAsyncActionBase
+{
+	GENERATED_BODY()
+
+public:
+
+	UPROPERTY(BlueprintAssignable, Category = "Omega")
+	FCooldownChangedSignature CooldownStart;
+
+	UPROPERTY(BlueprintAssignable, Category = "Omega")
+	FCooldownChangedSignature CooldownEnd;
+
+	UFUNCTION(BlueprintCallable, Category = "Omega", meta = (BlueprintInternalUseOnly = "true"))
+	static UWaitCooldownChange* WaitForCooldownChange(UAbilitySystemComponent* AbilitySystemComponent, const FGameplayTag& InCooldownTag);
+
+	UFUNCTION(BlueprintCallable, Category = "Omega", meta = (BlueprintInternalUseOnly = "true"))
+	void EndTask();
+
+protected:
+
+	void CooldownTagChanged(const FGameplayTag InCooldownTag, int32 NewCount);
+	void OnActiveEffectAdded(UAbilitySystemComponent* AbilitySystem, const FGameplayEffectSpec& AppliedSpec, FActiveGameplayEffectHandle ActiveEffectHandle);
+
+	UPROPERTY()
+	TObjectPtr<UAbilitySystemComponent> AbilitySystemComponent;
+	
+	FGameplayTag CooldownTag;
+};
